5c9fbd531899a387e58ecc0448264ef5fc917ec0
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-04 21:03:36 -0300

    feat(list): filtros na query de busca de medicos.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,10 +26,9 @@ public class MedicoController {
         repository.save(new Medico(dadosMedico));
     }
 
-
     @GetMapping
-    public List<DadosListagemMedico> getMedicos(){
-        return repository.findAll().stream()
-                .map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> getMedicos(Pageable paginacao){
+        return repository.findAll(paginacao)
+                .map(DadosListagemMedico::new);
     }
 }
