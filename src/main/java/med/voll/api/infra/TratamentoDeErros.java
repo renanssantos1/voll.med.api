9c2df0778d144303a1b88328a53acc5a01a584e2
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-09 08:55:22 -0300

    feat(try_errors) funcao para tratar erros de validacao 400.

--- src/main/java/med/voll/api/infra/TratamentoDeErros.java
+++ src/main/java/med/voll/api/infra/TratamentoDeErros.java
@@ -2,6 +2,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,5 +15,18 @@ public class TratamentoDeErros {
         return ResponseEntity.notFound().build();
     }
 
-    public TratamentoDeErros(){}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException exception){
+        var errors = exception.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosUsuarioValidacao::new).toList());
+    }
+
+    private record DadosUsuarioValidacao(String field, String message){
+
+        public DadosUsuarioValidacao(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+
+    }
 }
