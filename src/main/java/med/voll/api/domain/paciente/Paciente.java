1a1edf67b7d398a284d69a00155751d8abed81b8
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-10 20:16:05 -0300

    feat(camada_paciente) criado a entidade paciente.

---
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,37 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.domain.endereco.Endereco;
+
+@Table(name="pacientes")
+@Entity(name = "Paciente")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of="id")
+public class Paciente {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    private String cpf;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dadosPaciente){
+        this.nome = dadosPaciente.nome();
+        this.email = dadosPaciente.email();
+        this.cpf = dadosPaciente.cpf();
+        this.telefone = dadosPaciente.telefone();
+        this.endereco = new Endereco(dadosPaciente.endereco());
+    }
+}
