18aae101b53b390cfb425c5fdff85d1a0990b408
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-10 21:09:43 -0300

    feat(camada_paciente) devolvendo o DTO de cadastro do paciente no cadastro.

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -2,6 +2,7 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.paciente.DadosCadastroPaciente;
+import med.voll.api.domain.paciente.DadosDetalhamentoPaciente;
 import med.voll.api.domain.paciente.Paciente;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,10 @@ public class PacienteController {
     public ResponseEntity create(@RequestBody @Valid DadosCadastroPaciente dadosPaciente,
                                  UriComponentsBuilder uriComponentsBuilder){
         var paciente = new Paciente(dadosPaciente);
+
         var uri = uriComponentsBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
         repository.save(paciente);
 
-        return ResponseEntity.created(uri).body(paciente);
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoPaciente(paciente));
     }
 }
