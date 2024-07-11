f5e29ed773e7261381e768a00826f6270785c2ef
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-10 21:02:15 -0300

    feat(camada_paciente) add metodo de inserir paciente com validacao.

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,33 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.paciente.DadosCadastroPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity create(@RequestBody @Valid DadosCadastroPaciente dadosPaciente,
+                                 UriComponentsBuilder uriComponentsBuilder){
+        var paciente = new Paciente(dadosPaciente);
+        var uri = uriComponentsBuilder.path("/pacientes/{id}").buildAndExpand(paciente.getId()).toUri();
+        repository.save(paciente);
+
+        return ResponseEntity.created(uri).body(paciente);
+    }
+}
