0a0261120ef9878796f7728b0bb1cdcdd93041e8
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-13 11:33:08 -0300

    feat(token_jwt) removido o sout.

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -18,7 +18,6 @@ public class TokenService {
     private String secret;
 
     public String gerarToken(Usuario usuario) {
-        System.out.println(secret);
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
