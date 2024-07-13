91ef6d40323b12f4de5d0892914a2075312adddd
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-13 11:05:03 -0300

    feat(token_jwt) classe que ira gerar o token jwt.

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,18 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+
+
+}
