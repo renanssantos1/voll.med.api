5f74946bb105ecb77b33a4c9e088b71847a90c61
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-15 22:40:54 -0300

    feat(controle_de_acesso)get token authorization header.

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -8,13 +8,27 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
 
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (Objects.isNull(authorizationHeader)) {
+            throw new RuntimeException("Token não enviado no cabeçalho Authorization");
+        }
+
+        return authorizationHeader.replace("Bearer", "").trim();
     }
 
 }
