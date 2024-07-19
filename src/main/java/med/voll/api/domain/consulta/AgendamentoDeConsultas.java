736756c4e41a251bd9f62bc50d4dd9c234afebb7
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-18 21:32:44 -0300

    feat(agendamento_consulta) carregando medico e paciente no agendamento.

--- src/main/java/med/voll/api/domain/consulta/AgendamentoDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendamentoDeConsultas.java
@@ -1,10 +1,28 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.Paciente;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AgendamentoDeConsultas {
-    public void agendarConsulta(DadosAgendamentoConsulta dadosAgendamento){
 
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void agendarConsulta(DadosAgendamentoConsulta dadosAgendamento){
+        Paciente paciente = pacienteRepository.getReferenceById(dadosAgendamento.idPaciente());
+        Medico medico = medicoRepository.getReferenceById(dadosAgendamento.idMedico());
+
+        Consulta consulta = new Consulta(null, medico, paciente, dadosAgendamento.data());
     }
 }
