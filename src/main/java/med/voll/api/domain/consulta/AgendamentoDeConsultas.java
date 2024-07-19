3bffb989cec4d5440db2164a1b6c6ed7e3719fb8
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-18 21:26:55 -0300

    feat(agendamento_consulta) classe para regra de negocio consulta.

---
+++ src/main/java/med/voll/api/domain/consulta/AgendamentoDeConsultas.java
@@ -0,0 +1,10 @@
+package med.voll.api.domain.consulta;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendamentoDeConsultas {
+    public void agendarConsulta(DadosAgendamentoConsulta dadosAgendamento){
+
+    }
+}
