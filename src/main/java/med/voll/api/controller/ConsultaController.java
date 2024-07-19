75b2686cc160dbc837ec85d6ca052bb8eca7ebca
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-18 22:53:11 -0300

    feat(agendamento_consulta) injecao de dependencia do agd consulta.

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,8 +2,10 @@ package med.voll.api.controller;
 
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendamentoDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendamentoDeConsultas agenda;
+
+
     @PostMapping
     @Transactional
     public ResponseEntity agendarConsulta(@RequestBody @Valid DadosAgendamentoConsulta dadosAgendamento) {
+        agenda.agendarConsulta(dadosAgendamento);
+
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
