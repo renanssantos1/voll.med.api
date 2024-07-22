4d439446064b47fca2129a24abeb7b062636d64d
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 20:28:21 -0300

    feat(rules_validator) ValidadorHorarioAntecedencia adicionado.

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -0,0 +1,12 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioAntecedencia {
+
+    
+}
