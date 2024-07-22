a3abe3ac7cc42deacffef9abf99ef40294d3029b
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 20:35:21 -0300

    feat(rules_validator) metodo para buscar medico ativo.

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -0,0 +1,23 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.infra.exception.ValidacaoException;
+
+import java.util.Objects;
+
+public class ValidadorMedicoAtivo {
+
+    private MedicoRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        if(Objects.isNull(dados.idMedico())){
+            return;
+        }
+        boolean medicoAtivo = repository.findAtivoById(dados.idMedico());
+        if (!medicoAtivo){
+            throw new ValidacaoException("Consulta nao pode ser agendada com medico inexistente");
+        }
+    }
+
+}
