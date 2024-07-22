e01be376ebe684f4d249d2c1fadc45052bc9b3b1
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 20:22:30 -0300

    feat(rules_validator) validador horario de funcionamento.

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoCLinica.java
@@ -0,0 +1,15 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+
+public class ValidadorHorarioFuncionamentoCLinica {
+
+    public void validar(DadosAgendamentoConsulta dados) {
+
+
+    }
+
+}
