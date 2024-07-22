b297628ea2989faa8260436ee9d0cda51cfe5431
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 20:40:32 -0300

    feat(rules_validator) validador a disponibilidade do medico.

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoComOutraConsultaNoMesmoHorario.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+public class ValidadorMedicoComOutraConsultaNoMesmoHorario {
+
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados){
+        Boolean medicoPossuiOutraConsultaNoMesmoHorario = repository.existsByMedicoIdAndData(dados.idMedico(), dados.data());
+        if(medicoPossuiOutraConsultaNoMesmoHorario){
+            throw new ValidacaoException("Medico já possui agendamento nesse horario");
+        }
+
+    }
+
+}
