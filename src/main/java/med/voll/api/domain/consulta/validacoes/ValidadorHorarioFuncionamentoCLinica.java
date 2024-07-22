7dc9a127b97c23dd024e9414262fb6cddea00013
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 20:24:42 -0300

    feat(rules_validator) funcao para valida o horario funcionamento.

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoCLinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoCLinica.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.consulta.validacoes;
 
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
@@ -8,8 +9,14 @@ import java.time.LocalDateTime;
 public class ValidadorHorarioFuncionamentoCLinica {
 
     public void validar(DadosAgendamentoConsulta dados) {
+        LocalDateTime dadosConsulta = dados.data();
+        boolean isDomingo = dadosConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        boolean isAntesAberturaClinica = dadosConsulta.getHour() < 7;
+        boolean isClinicaFechada = dadosConsulta.getHour() > 18;
 
-
+        if (isDomingo || isAntesAberturaClinica || isClinicaFechada) {
+            throw new ValidacaoException("Consulta fora de horario de funcionamento");
+        }
     }
 
 }
