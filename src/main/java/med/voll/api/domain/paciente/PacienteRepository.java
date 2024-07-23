68f677ea3e708f55ff08486c3c7e705e8bf04527
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 23:21:34 -0300

    feat(rules_validator): Optional removido.

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByAtivoTrue(Pageable pagination);
 
