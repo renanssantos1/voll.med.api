da25abd3bfa4c2d1c23e64fa7615303458496d55
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 21:58:47 -0300

    feat(rules_validator) consulta horario paciente mesmo dia.

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.consulta.ConsultaRepository;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.infra.exception.ValidacaoException;
+
+import java.time.LocalDateTime;
+
+public class ValidadorPacienteSemOutraConsultaNoDia {
+
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        LocalDateTime primeiroHorario = dados.data().withHour(7);
+        LocalDateTime ultimoHorario = dados.data().withHour(18);
+        boolean isPacientePossuiConsulta = repository.existsByPacienteIdAndDataBetween(dados.idPaciente(),primeiroHorario, ultimoHorario);
+        if (isPacientePossuiConsulta) {
+            throw new ValidacaoException("Paciente ja possui consulta agendada para esse dia.");
+        }
+    }
+
+}
