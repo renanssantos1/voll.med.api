960d4c4a51c01b64fd666a20667ce21357ae76f9
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-22 22:53:31 -0300

    fix(rules_validator): ajuste query de buscar paciente ativo.

--- src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -3,11 +3,15 @@ package med.voll.api.domain.paciente;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
     Page<Paciente> findAllByAtivoTrue(Pageable pagination);
 
-    boolean findByAtivoById(Long idPaciente);
+    @Query("""
+            select p.ativo from Paciente p where p.id = :id
+            """)
+    boolean findByAtivoById(Long id);
 }
