c1a8b101efa3e8855da93e753c99fee44ab5ad9e
Renan da Silva Santos <renandasilvasantos557@gmail.com>
2024-07-29 20:31:48 -0300

    feat(test_api): implementacao da classe para teste.

---
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -0,0 +1,16 @@
+package med.voll.api.domain.medico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioLivreNaData() {
+    }
+}
